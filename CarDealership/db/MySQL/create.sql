d29b947c27a27e359032df7d7d396b31e45e9f0a
aluno <carloens@estudante.ufscar.br>
2022-07-18 16:04:48 -0300

    Tabela CLiente+lojas

--- CarDealership/db/MySQL/create.sql
+++ CarDealership/db/MySQL/create.sql
@@ -1,3 +1,7 @@
+drop database if exists CarDealershipSQL;
+
+create database CarDealershipSQL;
+
 create table Cliente(id bigint not null auto_increment ,
 e-mail varchar(50) not null,
 senha varchar(50) not null, 
@@ -6,3 +10,13 @@ nome varchar(50) not null,
 telefone integer not null, 
 sexo char, 
 dataDeNascimento date not null, primary key (id));
+
+create table lojas (
+id_loja int not null auto_increment,
+nome varchar(50) not null,
+email varchar(50) not null,
+senha varchar(50) not null,
+descricao varchar(60) not null,
+cnpj int(14) not null,
+primary key(id_loja)
+)
