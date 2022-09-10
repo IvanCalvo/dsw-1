1f87952554eb497f0d591e082dbba19743ee3501
Pedro Salviano <pedro.salviano@vonbraunlabs.com.br>
2022-09-10 16:40:17 -0300

    set authenticator config

--- trab3/CarDealership/src/main/java/dsw/CarDealership/config/WebSecurityConfig.java
+++ trab3/CarDealership/src/main/java/dsw/CarDealership/config/WebSecurityConfig.java
@@ -41,18 +41,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-				.antMatchers("/", "/index", "/error","/carros/listar").permitAll()
-				.antMatchers("/propostas/**").hasAnyAuthority("CLIENTE","LOJA")
-				.antMatchers("/usuarios/**", "/admin/**","/admins/**").hasAuthority("ADMIN")
-				.antMatchers("/lojas/**", "/carros/**").hasAnyAuthority("LOJA","ADMIN")
-				.antMatchers("/login/**", "/js/**", "/css/**", "/image/**", "/webjars/**").permitAll()
+				.antMatchers("/clientes", "/lojas").permitAll()
+				.antMatchers("/clientes/{\\d+}", "/lojas/{\\d+}").permitAll()
+				.antMatchers("/propostas/carros/{\\d+}").permitAll()
+				.antMatchers("/propostas/clientes/{\\d+}").permitAll()
+				.antMatchers("/carros/lojas/{\\d+}").permitAll()
+				.antMatchers("/carros/modelos/{\\w+}").permitAll()
 				.anyRequest().authenticated()
-			.and()
-				.formLogin()
-				.permitAll()
-			.and()
-				.logout()
-				.logoutSuccessUrl("/")
-				.permitAll();
+				.and().formLogin().loginPage("/login").permitAll()
+				.and().logout().logoutSuccessUrl("/").permitAll();
 	}
 }
